4b79c60d9f2bfc9eac9ca8f4fdfb8a916328b1e6
Luis Silva <luis.silva@radicalsystems.com.au>
2017-12-15 10:30:55 +0930

    Supports IOS and Material

--- src/services/Framework7Engine.ts
+++ src/services/Framework7Engine.ts
@@ -4,12 +4,17 @@ import {  singleton } from 'aurelia-framework';
 
 @singleton() 
 export class Framework7Engine {
-    
+ 
+    platform:Platform;
     instance: Framework7;
-
     mainView: Framework7.View;
-
+    
     setUpFramework7 () {
+      
+      console.log("This is plat:" + MOBILE_PLATFORM);
+      this.platform = (MOBILE_PLATFORM == "ios")? Platform.ios : Platform.android; 
+
+
       this.instance = new Framework7({
         material: true,
         animateNavBackIcon: true
@@ -20,4 +25,9 @@ export class Framework7Engine {
           main: true
       });
     }
+}
+
+export enum Platform {
+  ios = "IOS",
+  android = "Android"
 }
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,11 @@
 /// <reference path="framework7/dom7.d.ts" />
 /// <reference path="framework7/framework7.d.ts" />
 /// <reference path="framework7/template7.d.ts" />
+
+declare var MOBILE_PLATFORM:string;
+
+declare var require: {
+    <T>(path: string): T;
+    (paths: string[], callback: (...modules: any[]) => void): void;
+    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
+};
